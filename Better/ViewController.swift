870eb73db88c19e1b4185435fb9caaab2298c15d
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-02-08 23:04:06 -0500

    Initial Commit

---
+++ Better/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Better
+//
+//  Created by John Jakobsen on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
