1d428710947afe5be4bc119d06ff45d2584908f3
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2024-04-29 23:29:08 -0400

    Create Colors constants

---
+++ Better/Better/Constants/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  Better
+//
+//  Created by John Jakobsen on 4/16/24.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let blackTextColor: UIColor = .init(red: 53/256, green: 56/256, blue: 57/256, alpha: 1.0)
+    static let greyTextColor: UIColor = .init(red: 148/256, green: 160/256, blue: 165/256, alpha: 1.0)
+    static let greyBackgroundColor: UIColor = .init(red: 217/256, green: 217/256, blue: 217/256, alpha: 1.0)
+}
